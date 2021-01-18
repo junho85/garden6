302102397df702b36830fa421017a8c442aeb773
june.kim <junho85@gmail.com>
2021-01-19 01:19:51 +0900

    garden5 에서 복사 해 와서 세팅한 프로젝트

---
+++ README.md
@@ -0,0 +1,10 @@
+# garden6
+* 정원사들 시즌6 출석부입니다.
+* slack #commit 채널에 올라온 메시지들을 수집해서 출석부를 작성합니다.
+* [시즌 6 출석부 바로가기 gogo](http://garden6.junho85.pe.kr/)
+* [github](https://github.com/junho85/garden6)
+* [wiki](https://github.com/junho85/garden6/wiki)
+
+## 참고
+* [github](https://github.com/junho85/garden5)
+* [github](https://github.com/junho85/garden4)
---
+++ garden6/wsgi.py
@@ -0,0 +1,22 @@
+"""
+WSGI config for garden6 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.conf import settings
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'garden6.settings')
+
+if settings.DEBUG:
+    application = StaticFilesHandler(get_wsgi_application())
+else:
+    application = get_wsgi_application()
+
